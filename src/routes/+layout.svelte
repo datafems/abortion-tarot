2ba72441f5ca17085f07e3fb4a83736ca5b3c0f0
DataFems <datafems@gmail.com>
2026-03-16 02:42:11 +0700

    deploy github page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+
 	import favicon from '$lib/assets/favicon.png';
-	
 	// Styles
     import '$lib/styles/app.css';
 	let { children } = $props();
+
+	export const prerender = true;
+	export const trailingSlash = 'always';
 </script>
 
 <svelte:head>
