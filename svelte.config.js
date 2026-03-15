5199f711fac65d2f466aed1ce441bff722794c3e
DataFems <datafems@gmail.com>
2026-03-16 02:52:19 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
-    appDir: 'abortion-tarot', // avoids the underscore issue with GitHub Pages
+    appDir: 'app',
     paths: {
       base: process.env.BASE_PATH || '',
     },
   },
 };
-
 
 export default config;
