b80b5d96ddbf505022e86e261794747fbcf5535c
DataFems <datafems@gmail.com>
2026-03-16 12:41:02 +0700

    load font at first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,13 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+  	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+    href="https://fonts.googleapis.com/css2?family=Niramit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet"
+	/>
 </svelte:head>
 
 {@render children()}
